1838cc9ce62b04d68e04c5eeabe5c868a336acd5
IFTICENE <chermatti.31@gmail.com>
2026-03-08 16:59:06 -0400

    Creation de la premiere methode de la classe Emplyer

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
-class Employee:
+class Employer:
     def __init__(self, numpermis, nom, prenom):
         self.numpermis = numpermis
         self.nom = nom
         self.prenom = prenom
         self.voiture = None
+
+    def afficherInformations(self):
+        print("----- Employe -----")
+        print("Numero de permis :", self.numpermis)
+        print("Nom :", self.nom)
+        print("Prenom :", self.prenom)
+
+        if self.voiture is None:
+            print("Voiture de service : aucune")
+        else:
+            print("Voiture de service :", self.voiture.matricule)
