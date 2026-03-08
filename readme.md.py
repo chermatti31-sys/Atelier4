0a99d0364446bef31912885537d1f941ef21084e
IFTICENE <chermatti.31@gmail.com>
2026-03-08 16:43:33 -0400

    ajout et personnalisation de readme

---
+++ readme.md.py
@@ -0,0 +1,15 @@
+#POO-Atelier 4 : Gestion de voitures de service
+#Description
+
+Ce programme en Python simule la gestion des voitures de service dans une entreprise
+en utilisant la programmation orientée objet.
+Il contient deux classes : Employee pour représenter les employés et Voiture pour représenter les voitures.
+Chaque employé peut avoir une seule voiture de service et chaque voiture peut être attribuée à un seul employé.
+Le programme permet d’afficher les informations, d’attribuer une voiture et de la retirer.
+Des vérifications sont aussi faites pour éviter les attributions incorrectes.
+
+#information:
+Nom et prénom : Hacene Ifticene
+Cours : INF1093 – Programmation II
+Professeur : Youness Boukouchi
+Établissement : Collège Boréal
