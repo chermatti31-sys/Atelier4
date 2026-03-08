ca28f9e09bf8deb85b0720dd4a9ac1147891a5ee
IFTICENE <chermatti.31@gmail.com>
2026-03-08 17:01:21 -0400

    Affectation d’une voiture à un employé

--- main.py
+++ main.py
@@ -15,3 +15,13 @@ class Employer:
             print("Voiture de service : aucune")
         else:
             print("Voiture de service :", self.voiture.matricule)
+
+    def affecterVoiture(self, voiture):
+        if self.voiture is not None:
+            print("Erreur : cet employe possede deja une voiture.")
+        elif voiture.chauffeur is not None:
+            print("Erreur : cette voiture est deja attribuee.")
+        else:
+            self.voiture = voiture
+            voiture.chauffeur = self
+            print("Voiture affectee avec succes.")
